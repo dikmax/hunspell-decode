ce44911d7ea67ad855590f1c03de0150f588a0ca
Maxim Dikun <mdzikun@scnsoft.com>
2012-01-25 16:41:22 +0300

    Rewrite

--- decode.php
+++ decode.php
@@ -7,6 +7,8 @@ class Decoder
     private $_dicFile;
     private $_resFile;
 
+    private $_affixes;
+
     public function __construct()
     {
         $this->_affFile = fopen('ru_RU.aff', 'r');
@@ -24,7 +26,7 @@ class Decoder
     {
         echo "Reading .aff file\n";
 
-        $affixes = array();
+        $this->_affixes = array();
         $prevType = $prevIndex = null;
 
         while ($line = fgets($this->_affFile)) {
@@ -48,13 +50,13 @@ class Decoder
                     $prevType = $type;
                     $prevIndex = $index;
 
-                    $affixes[$index] = array(
+                    $this->_affixes[$index] = array(
                         'type' => $type == 'SFX',
                         'connection' => $command[2],
                         'rules' => array()
                     );
                 } else {
-                    $affixes[$index]['rules'][] = array(
+                    $this->_affixes[$index]['rules'][] = array(
                         'remove' => $command[2] == '0' ? '.' : $command[2],
                         'affix' => $command[3] == '0' ? '.' : $command[3],
                         'condition' => $command[4] == '0' ? '.' : $command[4]
@@ -82,44 +84,115 @@ class Decoder
             }
             list($word, $flagsString) = explode('/', $line, 2);
             $flags = str_split($flagsString, 2);
-            $prefixes = array(array(array('condition' => '.', 'remove' => '.', 'affix' => '.')));
-            $suffixes = array(array(array('condition' => '.', 'remove' => '.', 'affix' => '.')));
+            /*$prefixes = array(array(array('condition' => '.', 'remove' => '.', 'affix' => '.')));
+            $suffixes = array(array(array('condition' => '.', 'remove' => '.', 'affix' => '.')));*/
+            $prefixes = array();
+            $suffixes = array();
             foreach ($flags as $flag) {
-                if (isset($affixes[$flag])) {
-                    if ($affixes[$flag]['type']) {
-                        $suffixes[] = $affixes[$flag]['rules'];
+                if (isset($this->_affixes[$flag])) {
+                    if ($this->_affixes[$flag]['type']) {
+                        $suffixes[$flag] = $this->_affixes[$flag]['rules'];
                     } else {
-                        $prefixes[] = $affixes[$flag]['rules'];
+                        $prefixes[$flag] = $this->_affixes[$flag]['rules'];
                     }
                 }
             }
 
-            $newWords = array();
-            foreach ($suffixes as $suffix) {
-                foreach ($suffix as $rule) {
-                    $newWord = $word;
-                    if ($rule['condition'] != '.' || !preg_match('/' . $rule['condition'] . '$/u', $newWord)) {
-                        if ($rule['remove'] != '.') {
-                            $newWord = preg_replace('/' . $rule['remove'] . '$/u', '', $newWord);
-                        }
-                        if ($rule['affix'] != '.') {
-                            $newWord .= $rule['affix'];
-                        }
-                    } else {
-                        continue;
-                    }
-                    $newWords[] = $newWord;
+            $result = array($word);
+            foreach ($suffixes as $flag => $suffix) {
+                $newWords = $this->_applySuffix($word, $suffix);
+                $result = array_merge($result, $newWords);
+                foreach ($prefixes as $prefix) {
+                    $newWords2 = $this->_applyPrefix($newWords, $prefix, $flag);
+                    $result = array_merge($result, $newWords2);
                 }
-                /*foreach ($prefixes as $prefix) {
-
-                }*/
             }
-            $newWords = array_unique($newWords);
-            foreach ($newWords as $newWord) {
+
+            foreach ($prefixes as $flag => $prefix) {
+                $newWords = $this->_applyPrefix($word, $prefix);
+                $result = array_merge($result, $newWords);
+                foreach ($suffixes as $suffix) {
+                    $newWords2 = $this->_applySuffix($newWords, $suffix, $flag);
+                    $result = array_merge($result, $newWords2);
+                }
+            }
+
+            $result = array_unique($result);
+            foreach ($result as $newWord) {
                 $this->_writeWord($newWord);
             }
 
         }
+    }
+
+    private function _applySuffix($words, $suffix, $prefixFlag = null)
+    {
+        if (!is_array($words)) {
+            $words = array($words);
+        }
+        $result = array();
+        foreach ($words as $word) {
+            foreach ($suffix as $rule) {
+                if (strpos($rule['affix'], '/') !== false) {
+                    list($replace, $flagsString) = explode('/', $rule['affix'], 2);
+                    $flags = str_split($flagsString, 2);
+                    if (!in_array($prefixFlag, $flags)) {
+                        continue;
+                    }
+                } else {
+                    $replace = $rule['affix'];
+                }
+                $newWord = $word;
+                if ($rule['condition'] == '.' || preg_match('/' . $rule['condition'] . '$/u', $newWord)) {
+                    if ($rule['remove'] != '.') {
+                        $newWord = preg_replace('/' . $rule['remove'] . '$/u', '', $newWord);
+                    }
+                    if ($replace != '.') {
+                        $newWord .= $replace;
+                    }
+                } else {
+                    continue;
+                }
+                $result[] = $newWord;
+            }
+        }
+
+        return array_unique($result);
+    }
+
+    private function _applyPrefix($words, $prefix, $suffixFlag = null)
+    {
+        if (!is_array($words)) {
+            $words = array($words);
+        }
+        $result = array();
+        foreach ($words as $word) {
+            foreach ($prefix as $rule) {
+                if (strpos($rule['affix'], '/') !== false) {
+                    list($replace, $flagsString) = explode('/', $rule['affix'], 2);
+                    $flags = str_split($flagsString, 2);
+                    if (!in_array($suffixFlag, $flags)) {
+                        continue;
+                    }
+                } else {
+                    $replace = $rule['affix'];
+                }
+                $newWord = $word;
+                if ($rule['condition'] == '.' || preg_match('/^' . $rule['condition'] . '/u', $newWord)) {
+                    if ($rule['remove'] != '.') {
+                        $newWord = preg_replace('/^' . $rule['remove'] . '/u', '', $newWord);
+                    }
+                    if ($replace != '.') {
+                        $newWord = $replace . $newWord;
+                    }
+                } else {
+                    continue;
+                }
+                $result[] = $newWord;
+            }
+        }
+
+        return array_unique($result);
     }
 
     private function _writeWord($word) {
